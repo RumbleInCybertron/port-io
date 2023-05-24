abd23e0ea57454c9a97e024b7d097e27f424ac6d
Derek <rumbleincybertron@gmail.com>
2023-05-24 18:11:29 +0900

    update next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     serverActions: true,
   },
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: "/public/myfile.html",
+      destination: "/pages/api/myfile.js",
+    },
+  ],
+  output: 'standalone',
+  images: {domains:["avatars.githubusercontent.com", "lh3.googleusercontent.com"], formats: ['image/avif', 'image/webp'],}
 }
 
 module.exports = nextConfig
