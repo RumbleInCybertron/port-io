138742a5d06dce7a7a44e5874dbaf89e98a6ab3b
Derek <rumbleincybertron@gmail.com>
2023-05-26 18:55:27 +0900

    + credentials provider to auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,7 @@
-import { NextAuthOptions } from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
+import { compare } from "bcryptjs";
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from '@/lib/prisma';
@@ -12,6 +14,39 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
   },
   providers: [
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !(await compare(credentials.password, user.password))) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          randomKey: "Hey cool",
+        };
+      },
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
@@ -21,6 +56,29 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
+  callbacks: {
+    session: ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey,
+        },
+      };
+    },
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET
 }
