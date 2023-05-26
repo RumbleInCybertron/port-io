0ceae2c88871c3bd5d250392cb8031667913019d
Derek <rumbleincybertron@gmail.com>
2023-05-26 20:42:39 +0900

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,31 @@ module.exports = {
   important: true,
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: "#ad20c9",
-        secondary: "#e60e84"
-      }
+        secondary: "#e60e84",
+        'ct-dark-600': '#222',
+        'ct-dark-200': '#e5e7eb',
+        'ct-dark-100': '#f5f6f7',
+        'ct-blue-600': '#2363eb',
+        'ct-yellow-600': '#f9d13e',
+      },
+      fontFamily: {
+        Poppins: ['Poppins, sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          lg: '1125px',
+          xl: '1125px',
+          '2xl': '1125px',
+        },
+      },
     },
   },
   darkMode: 'class',
