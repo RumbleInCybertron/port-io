ece6b636f952fbbb469c867bd2be3d3703175571
Derek <rumbleincybertron@gmail.com>
2023-05-26 18:53:53 +0900

    + home init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import {
+  LoginButton,
+  LogoutButton,
+  ProfileButton,
+  RegisterButton,
+} from "@/components/Buttons";
+import Navbar from '@/components/Navbar';
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">ホームページをこちらに埋め込んで下さいませ</div>
+    </>
+  );
 }
