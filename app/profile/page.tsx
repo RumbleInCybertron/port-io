04224eec28a711cbab7cd15bb448cddf31e24e9f
Derek <rumbleincybertron@gmail.com>
2023-05-29 20:46:20 +0900

    profile pg init

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,42 @@
 import '@/app/styles/globals.css'
+import { getServerSession } from "next-auth";
+import Navbar from "@/components/Navbar";
+import { authOptions } from "@/lib/auth";
+import Image from "next/image";
 
-export default function Page() {
-  return <h1>Second route!</h1>;
+export default async function Profile() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  return (
+    <>
+      <Navbar />
+      <section className="bg-ct-blue-600  min-h-screen pt-20">
+        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
+          <div>
+            <p className="mb-3 text-5xl text-center font-semibold">
+              Profile Page
+            </p>
+            {!user ? (
+              <p>Loading...</p>
+            ) : (
+              <div className="flex items-center gap-8">
+                <div>
+                  <Image
+                    src={user.image ? user.image : "/images/default.png"}
+                    className="max-h-36"
+                    alt={`profile photo of ${user.name}`}
+                  />
+                </div>
+                <div className="mt-8">
+                  <p className="mb-3">Name: {user.name}</p>
+                  <p className="mb-3">Email: {user.email}</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
