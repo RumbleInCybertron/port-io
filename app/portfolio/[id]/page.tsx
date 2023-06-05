94c3fd6acfa9d4c78901cf8cf0760c1e0ac53364
Derek <rumbleincybertron@gmail.com>
2023-06-05 19:23:12 +0900

    + portfolio page

---
+++ app/portfolio/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { GetPortfolio } from "@/components/Portfolio";
+
+export default async function PortfolioPage({ params }: { params: { id: string } }) {
+  const portfolio = await prisma.portfolio.findUniqueOrThrow({
+    where: { id: String(params.id) },
+    select: {
+      id: true,
+      userId: true,
+      name: true,
+      stockAssets: { select: { name: true, ticker: true, index: true, amount: true, average: true, updatedAt: true, id: true } },
+      cryptoAssets: { select: { name: true, ticker: true, amount: true, average: true, updatedAt: true, id: true } }
+    }
+  });
+
+  console.log("Portfolio: ", portfolio);
+  return (
+    <GetPortfolio {...portfolio} />
+  )
+};
