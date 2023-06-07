be35fc18ab1c2dd049bfbd20065fd2ba8be37cab
Derek <rumbleincybertron@gmail.com>
2023-06-07 22:47:43 +0900

    fix asset PUT route

--- app/api/portfolio/asset/route.ts
+++ app/api/portfolio/asset/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: Request) {
   try {
-    const { id, type, name, ticker, index, amount, price } = (await req.json()) as {
+    const { id, type, name, ticker, index, amount, price, portfolioId } = (await req.json()) as {
       id: string;
       type: string;
       name: string;
@@ -11,28 +11,29 @@ export async function PUT(req: Request) {
       index: string;
       amount: number;
       price: number;
+      portfolioId: string;
     };
 
     if(type === "stock") {
       const data = await prisma.stockAsset.findFirst({
         where: {
-          AND: [{ name: name}, {portfolioId: id}]
+          AND: [{ name: { equals: String(name)}}, {portfolioId: {equals: String(portfolioId)}}]
         },
         select: {id: true, amount: true, average: true}
       });
 
       if (data !== null && data.average > 0.00 && data.amount > 0){
-        const ttl_amt = data.amount + amount;
+        const ttl_amt = data.amount + Number(amount);
         console.log("Total Amount: ", ttl_amt);
-        const average = ((data.average * data.amount) + (price + amount)) / ttl_amt;
+        const average = ((data.average * data.amount) + (Number(price) * Number(amount))) / ttl_amt;
         const stock = await prisma.stockAsset.update({
           where: {id: id},
           data: {
-            portfolioId: data.id,    
+            portfolioId,    
             name,
             ticker,
             index,
-            amount: Number(ttl_amt),
+            amount: ttl_amt,
             average: Number(average),
           },
         });
@@ -40,7 +41,7 @@ export async function PUT(req: Request) {
       } else if (data === null) {
         const stock = await prisma.stockAsset.create({
           data: {
-            portfolioId: id,    
+            portfolioId: portfolioId,    
             name,
             ticker,
             index,
@@ -54,21 +55,21 @@ export async function PUT(req: Request) {
     } else if (type === "crypto") {
       const data = await prisma.cryptoAsset.findFirst({
         where: {
-          AND: [{ name: name}, {portfolioId: id}]
+          AND: [{ name: { equals: String(name)}}, {portfolioId: {equals: String(portfolioId)}}]
         },
         select: {id: true, amount: true, average: true}
       });
 
       if (data !== null && data.average > 0.00 && data.amount > 0){
-        const ttl_amt = data.amount + amount;
-        const average = ((data.average * data.amount) + (price + amount)) / ttl_amt;
+        const ttl_amt = data.amount + Number(amount);
+        const average = ((data.average * data.amount) + (Number(price) * Number(amount))) / ttl_amt;
         const crypto = await prisma.cryptoAsset.update({
           where: {id: data.id},
           data: {
-            portfolioId: id,    
+            portfolioId,    
             name,
             ticker,
-            amount: Number(ttl_amt),
+            amount: ttl_amt,
             average: Number(average),
           },
         });
@@ -76,7 +77,7 @@ export async function PUT(req: Request) {
       } else if (data === null) {
         const stock = await prisma.cryptoAsset.create({
           data: {
-            portfolioId: id,    
+            portfolioId: portfolioId,    
             name,
             ticker,
             amount: Number(amount),
@@ -99,58 +100,58 @@ export async function PUT(req: Request) {
   }
 }
 
-export async function GET(req: NextRequest) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const id = searchParams.get('id');
-    if(id === null) return NextResponse.json(JSON.stringify({status: "error", message: "id is null idiot"}), {status: 403})
+// export async function GET(req: NextRequest) {
+//   try {
+//     const { searchParams } = new URL(req.url);
+//     const id = searchParams.get('id');
+//     if(id === null) return NextResponse.json(JSON.stringify({status: "error", message: "id is null idiot"}), {status: 403})
 
-    const stocks = await prisma.stockAsset.findMany({
-      where: { portfolioId: String(id)},
-      select: {
-        name: true,
-        ticker: true,
-        index: true,
-        amount: true,
-        average: true,
-        portfolio: {
-          select: {
-            id: true
-          }
-        },
-      }
-    });
+//     const stocks = await prisma.stockAsset.findMany({
+//       where: { portfolioId: String(id)},
+//       select: {
+//         name: true,
+//         ticker: true,
+//         index: true,
+//         amount: true,
+//         average: true,
+//         portfolio: {
+//           select: {
+//             id: true
+//           }
+//         },
+//       }
+//     });
 
-    const cryptos = await prisma.cryptoAsset.findMany({
-      where: { portfolioId: String(id)},
-      select: {
-        name: true,
-        ticker: true,
-        amount: true,
-        average: true,
-        portfolio: {
-          select: {
-            id: true
-          }
-        },
-      }
-    });
+//     const cryptos = await prisma.cryptoAsset.findMany({
+//       where: { portfolioId: String(id)},
+//       select: {
+//         name: true,
+//         ticker: true,
+//         amount: true,
+//         average: true,
+//         portfolio: {
+//           select: {
+//             id: true
+//           }
+//         },
+//       }
+//     });
 
-    // TODO: validate stocks&cryptos against user id
+//     // TODO: validate stocks&cryptos against user id
  
-    return NextResponse.json({
-        assets: {
-          stocks,
-          cryptos
-        }
-      });
-  } catch (error: any) {
-    return new NextResponse(
-      JSON.stringify({
-        status: "error",
-        message: error.message,
-      }),
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({
+//         assets: {
+//           stocks,
+//           cryptos
+//         }
+//       });
+//   } catch (error: any) {
+//     return new NextResponse(
+//       JSON.stringify({
+//         status: "error",
+//         message: error.message,
+//       }),
+//       { status: 500 }
+//     );
+//   }
+// }
