e040585525519653d35509bf7d6de76cefa32fce
Derek <rumbleincybertron@gmail.com>
2023-06-07 22:49:26 +0900

    update update asset uri

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -16,6 +16,7 @@ export type PortfolioProps = {
 
 export const GetPortfolio = (portfolio: PortfolioProps) => {
   const router = useRouter();
+  const uri = "/portfolio/asset/update/" + portfolio.id
   return (
     <>
       <Navbar />
@@ -46,7 +47,7 @@ export const GetPortfolio = (portfolio: PortfolioProps) => {
               : (
                 <div>No Crypto Assets in this Portfolio</div>
               )}
-          <button className="m-2 w-1/3" onClick={() => router.push(`/portfolio/asset/update/?id=${portfolio.id}`)}>+ Add/Update Asset</button>
+          <button className="m-2 w-1/3" onClick={() => router.push(uri)}>+ Add/Update Asset</button>
         </div>
         {/* <StockAsset id={portfolio.id} name={portfolio.name} ticker={portfolio.ticker} amount={portfolio.amount} average={portfolio.average} updatedAt={portfolio.updatedAt} /> */}
       </div>
@@ -66,6 +67,32 @@ export const GetPortfolio = (portfolio: PortfolioProps) => {
           </div>
         ))}
       </div> */}
+}
+
+export const GetPortfolios = ({portfolios}: {portfolios: PortfolioProps[]}) => {
+  const router = useRouter();
+  return (
+    <>
+      <Navbar />
+      <div className="m-2 font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        Portfolios
+        {portfolios.length < 1
+          ? (
+            <div>
+              <div className="p-2 font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-pink-400 to-purple-600">No portfolios here. Would you like to add one?</div>
+            </div>
+          )
+          : (
+            portfolios.map((portfolio: PortfolioProps, i) => (
+              <div key={i} className="bg-purple-800/50 shadow ease-in duration-100 hover:shadow hover:bg-purple-900/50 mb-2 p-3 w-1/3">
+                <Portfolio {...portfolio} />
+              </div>
+            ))
+          )}
+        <button onClick={() => router.push("/portfolio/create")}>+ add</button>
+      </div>
+    </>
+  )
 }
 
 export const Portfolio = ({ ...portfolio }: PortfolioProps) => {
