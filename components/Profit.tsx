c5ebae4ff1f0c7f9fe39dc7e51ca6632eb80ce09
Derek <rumbleincybertron@gmail.com>
2023-06-16 19:33:53 +0900

    + profit cmpnt

---
+++ components/Profit.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export type ProfitProps = {
+  id: string;
+  amount: number;
+  stockAssetId: string | null;
+  cryptoAssetId: string | null;
+}
+
+export const Profit = ({ id, amount, stockAssetId, cryptoAssetId }: ProfitProps) => {
+  return (
+    <div className="text-inherit p-2">
+      <small>Amount: ${amount}</small>
+    </div>
+  )
+}
