55bcd7506278a723552d070b3e2e7bca6846ed5a
Derek <rumbleincybertron@gmail.com>
2023-06-16 19:34:36 +0900

    + callback uri + fiat to init port creation

--- app/portfolio/create/form.tsx
+++ app/portfolio/create/form.tsx
@@ -1,20 +1,28 @@
 "use client";
 
 import { ChangeEvent, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export const PortfolioForm = () => {
   const router = useRouter();
+  const { data: session, status } = useSession();
+  const user = session?.user;
+  
   const [formValues, setFormValues] = useState({
     name: "",
+    fiat: 0,
   });
   const [error, setError] = useState("");
 
+  const searchParams = useSearchParams();
+  const uri = "/portfolios/" + user?.email;
+  const callbackUrl = searchParams.get("callbackUrl") || uri;
+
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setFormValues({ name: "" });
+    setFormValues({ name: "", fiat: 0, });
 
-    try {
       const res = await fetch("/api/portfolio", {
         method: "POST",
         body: JSON.stringify(formValues),
@@ -28,9 +36,7 @@ export const PortfolioForm = () => {
         return;
       }
 
-    } catch (error: any) {
-      setError(error);
-    }
+      await router.push(callbackUrl);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -56,9 +62,19 @@ export const PortfolioForm = () => {
             name="name"
             className="text-black mb-2"
           />
-          <input 
-            disabled={!formValues.name} 
-            type="submit" 
+          <label htmlFor="fiat">Fiat</label>
+          <input
+            autoFocus
+            value={formValues.fiat !== null ? formValues.fiat : 0}
+            onChange={handleChange}
+            placeholder="e.g. '$12,345.67'"
+            type="number"
+            name="fiat"
+            className="text-black mb-2"
+          />
+          <input
+            disabled={!formValues.name}
+            type="submit"
             value="Create"
             className="bg-pink-400 mb-2"
           />
