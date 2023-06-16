681435b2a4a176330d5efb0a9c2de0b1958d5be3
Derek <rumbleincybertron@gmail.com>
2023-06-16 19:36:33 +0900

    ch create port to trans(added fiat creation)

--- app/api/portfolio/route.ts
+++ app/api/portfolio/route.ts
@@ -3,10 +3,31 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+async function create(userId: string, name: string, fiat: number) {
+  return await prisma.$transaction(async (tx) => {
+    const portfolio = await tx.portfolio.create({
+      data: {
+        userId: userId,
+        name: name,
+        ttl_value: fiat !== null ? Number(fiat) : 0,
+      },
+    });
+
+    await tx.fiat.create({
+      data: {
+        amount: fiat !== null ? Number(fiat) : 0,
+        portfolioId: portfolio.id,
+      },
+    });
+    return portfolio;
+  });
+}
+
 export async function POST(req: Request) {
   try {
-    const { name } = (await req.json()) as {
+    const { name, fiat } = (await req.json()) as {
       name: string;
+      fiat: number;
     };
     const session = await getServerSession(authOptions);
     const user = await prisma.user.findUniqueOrThrow({
@@ -14,17 +35,14 @@ export async function POST(req: Request) {
       select: { id: true },
     });
 
-    const portfolio = await prisma.portfolio.create({
-      data: { 
-        userId: user.id, 
-        name: name,
-      },
-    });
+    const portfolio = await create(user.id, name, fiat);
 
+    // TODO: if response not needed, remove
     return NextResponse.json({
       portfolio: {
         name: portfolio.name,
-      }
+        ttl_value: portfolio.ttl_value,
+      },
     });
   } catch (error: any) {
     return new NextResponse(
