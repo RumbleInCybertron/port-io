74ee357f8d1124e8bb99c194fbd2d06952336f3f
Derek <rumbleincybertron@gmail.com>
2023-06-16 19:35:33 +0900

    pass profit props + port props to port pg

--- app/portfolio/[id]/page.tsx
+++ app/portfolio/[id]/page.tsx
@@ -7,14 +7,24 @@ export default async function PortfolioPage({ params }: { params: { id: string }
       id: true,
       userId: true,
       name: true,
+      ttl_value: true,
       Fiat: true,
       stockAssets: { select: { name: true, ticker: true, index: true, amount: true, average: true, updatedAt: true, id: true } },
       cryptoAssets: { select: { name: true, ticker: true, amount: true, average: true, updatedAt: true, id: true } }
     }
   });
 
+  const stockAssetIds = portfolio.stockAssets.map(e => e.id);
+  const cryptoAssetIds = portfolio.cryptoAssets.map(e => e.id);
+  const profits = await prisma.profit.findMany({
+    where: { OR: [{ stockAssetId: { in: stockAssetIds }, cryptoAssetId: { in: cryptoAssetIds } }] },
+    select: {id: true, amount: true, stockAssetId: true, cryptoAssetId: true}
+  });
+
+  const props = {portfolio, profits};
+
   console.log("Portfolio w/ ID param: ", portfolio);
   return (
-    <GetPortfolio {...portfolio} />
+    <GetPortfolio {...props} />
   )
 };
