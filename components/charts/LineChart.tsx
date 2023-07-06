8454ef37ecb6a4101a30a73dd96e4d631a23b5e2
Derek <rumbleincybertron@gmail.com>
2023-07-06 15:18:42 +0900

    comment out line chart elements

--- components/charts/LineChart.tsx
+++ components/charts/LineChart.tsx
@@ -32,20 +32,21 @@ type DataProps = {
 
 export function LineChart(data: DataProps) {
   const options = {
+    responsive: true,
     plugins: {
       legend: {
         display: false,
       },
     },
-    elements: {
-      line: {
-        tension: 0,
-        borderWidth: 2,
-        borderColor: "rgba(47,97,68,1)",
-        fill: "start",
-        backgroundColor: "rgba(47,97,68,0.3)",
-      },
-    },
+    // elements: {
+    //   line: {
+    //     tension: 0,
+    //     borderWidth: 2,
+    //     borderColor: "rgba(47,97,68,1)",
+    //     fill: "start",
+    //     backgroundColor: "rgba(47,97,68,0.3)",
+    //   },
+    // },
     scales: {
       xAxis: {
         display: false,
