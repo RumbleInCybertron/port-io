3fb0a7f2a86c54d3601324a33e51dae7c8be70ae
Derek <rumbleincybertron@gmail.com>
2023-07-07 19:41:35 +0900

    profits two digits

--- components/Profit.tsx
+++ components/Profit.tsx
@@ -10,7 +10,7 @@ export type ProfitProps = {
 export const Profit = ({ id, amount, stockAssetId, cryptoAssetId }: ProfitProps) => {
   return (
     <div className="text-inherit p-2">
-      <small>Amount: ${amount}</small>
+      <small>Amount: ${amount.toFixed(2)}</small>
     </div>
   )
 }
