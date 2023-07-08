d16b502e23a5ac26a0e20b9782f9e6b9ae74fcc5
Derek <rumbleincybertron@gmail.com>
2023-07-08 19:38:05 +0900

    update getDates util

--- utils/getDates.ts
+++ utils/getDates.ts
@@ -1,12 +1,14 @@
-export const getDates = (length: number) => {
+export const getDates = (start: Date, end: Date) => {
     let dates = [];
     let temp;
-    let date = new Date();
-    for (let i = 0; i < length; i++) {
-      date.setDate(date.getDate() - 1);
+    let date = new Date(start);
+    const timeDiff = end.getTime() - start.getTime();
+    const dayDiff = timeDiff / (1000 * 3600 * 24);
+    dates.push(date.toLocaleDateString('en-US'));
+    for (let i = 0; i < dayDiff; i++) {
+      date.setDate(date.getDate()+1);
       temp = date.toLocaleDateString('en-US');
       dates.push(temp);
     }
-    dates.reverse();
     return dates;
 }
