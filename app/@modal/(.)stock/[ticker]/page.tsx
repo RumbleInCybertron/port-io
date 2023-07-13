88e0b985b24536781dcce2e9a5606697f5ffa50e
Derek <rumbleincybertron@gmail.com>
2023-07-13 19:42:07 +0900

    + todo

--- app/@modal/(.)stock/[ticker]/page.tsx
+++ app/@modal/(.)stock/[ticker]/page.tsx
@@ -1,6 +1,7 @@
 import '@/app/styles/globals.css'
 import Modal from "@/components/Modal";
 import { Selector } from "@/components/Selector";
+import Link from 'next/link';
 
 export default async function StockSelectPage() {
   const stocks = await prisma.stock.findMany({
@@ -10,6 +11,8 @@ export default async function StockSelectPage() {
   return (
     <Modal>
       <Selector {...stocks} />
+      {/*TODO: Links do not work for now the interceptor doesn't unmount and doesn't relinquish up control}
+      {/*<Link href="stock/NVDA" replace><span>Go!</span></Link>*/}
     </Modal>
   );
 }
