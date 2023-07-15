547a79373e910c11c056822f94789ee355fd35d6
Derek <rumbleincybertron@gmail.com>
2023-07-15 16:50:00 +0900

    + cyber icons pkg

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -3,12 +3,13 @@
 import { Collapse, Text } from "@nextui-org/react";
 import Portfolio, { PortfolioProps } from "@/components/Portfolio";
 import { useRouter } from "next/navigation";
+import { CyberEl42 } from 'react-cyber-elements'
 
 export default function Accordion(portfolios: PortfolioProps[]) {
   const router = useRouter();
   const pArr = Object.values(portfolios).sort();
   return (
-    <div className="m-2 font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+    <div className="m-2 font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 w-96">
       Portfolios
       {pArr.length < 1
         ? (
@@ -19,7 +20,10 @@ export default function Accordion(portfolios: PortfolioProps[]) {
         : (
           pArr.map((portfolio: PortfolioProps, i: any) => (
             <Collapse.Group key={i}>
-              <Collapse title={portfolio.name}>
+              <Collapse bordered  className="custom border-solid border-2 border-sky-500 text-transparent" title={portfolio.name} arrowIcon={<CyberEl42       style={{
+        width: '30px',
+        height: '30px'
+      }} />}>
                 <div className="bg-purple-800/50 shadow ease-in duration-100 hover:shadow hover:bg-purple-900/50 mb-2 p-3 w-1/3">
                   <Portfolio {...portfolio} />
                 </div>
