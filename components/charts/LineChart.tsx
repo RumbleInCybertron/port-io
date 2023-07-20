e02cdfec8557f48acd5ccda2cd72a65d396f945b
Derek <rumbleincybertron@gmail.com>
2023-07-20 18:50:10 +0900

    ch linechart + gradient

--- components/charts/LineChart.tsx
+++ components/charts/LineChart.tsx
@@ -11,6 +11,7 @@ import {
   Legend,
   Filler,
 } from "chart.js";
+import { useEffect, useRef, useState } from "react";
 
 ChartJS.register(
   CategoryScale,
@@ -23,7 +24,9 @@ ChartJS.register(
   Filler,
 );
 
-import { Line } from "react-chartjs-2";
+import { Chart, Line } from "react-chartjs-2";
+import { ChartData, ChartArea } from 'chart.js';
+import { faker } from '@faker-js/faker';
 
 type DataProps = {
   labels: string[];
@@ -32,7 +35,56 @@ type DataProps = {
   height: number;
 }
 
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'lime',
+  'green',
+  'teal',
+  'blue',
+  'purple',
+];
+
+function createGradient(ctx: CanvasRenderingContext2D, area: ChartArea) {
+  const colorStart = faker.helpers.arrayElement(colors);
+  console.log("colorStart", colorStart);
+  const colorMid = faker.helpers.arrayElement(
+    colors.filter(color => color !== colorStart)
+  );
+  const colorEnd = faker.helpers.arrayElement(
+    colors.filter(color => color !== colorStart && color !== colorMid)
+  );
+
+  const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+  gradient.addColorStop(0, colorStart);
+  gradient.addColorStop(0.5, colorMid);
+  gradient.addColorStop(1, colorEnd);
+
+  return gradient;
+}
+
 export function LineChart(data: DataProps) {
+  const chartRef = useRef<ChartJS>(null);
+  const [chartData, setChartData] = useState<ChartData<'line'>>({
+    datasets: [],
+  });
+
+  useEffect(() => {
+    const chart = chartRef.current;
+    if (!chart) return;
+
+    const chartData = {
+      ...data,
+      datasets: data.datasets.map(dataset => ({
+        ...dataset,
+        borderColor: createGradient(chart.ctx, chart.chartArea),
+      })),
+    };
+    setChartData(chartData);
+  }, [data]);
+
   const options = {
     responsive: true,
     plugins: {
@@ -52,14 +104,20 @@ export function LineChart(data: DataProps) {
     scales: {
       xAxis: {
         display: false,
+        gridLines: {
+          color: "red"
+        }
       },
       y: {
+        gridLines: {
+          color: "red",
+        },
         display: true,
         ticks: {
           callback: function (value: any) {
             return '$' + value;
           }
-        }
+        },
       },
     },
   }
@@ -67,9 +125,9 @@ export function LineChart(data: DataProps) {
   return (
     <>
       {/* line chart */}
-      <div className="container bg-slate-800 border-solid border-2 border-sky-500 rounded">
-        <div className="w-[150px] mx-auto mt-10 text-xl">ラインチャート</div>
-        <Line data={data} width={data.width} height={data.height} options={options} />
+      <div className="container bg-black border-solid border-2 border-sky-500 rounded">
+        <div className="w-[150px] mx-auto mt-10 text-yellow-100 text-xl">ラインチャート</div>
+        <Chart ref={chartRef} type="line" data={chartData} width={data.width} height={data.height} options={options} />
       </div>
     </>
   );
