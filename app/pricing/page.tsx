72024eb27067c5c6368c858e340c6c0b184fd9a6
Derek <rumbleincybertron@gmail.com>
2023-07-27 16:44:56 +0900

    + pricing pg init

---
+++ app/pricing/page.tsx
@@ -0,0 +1,10 @@
+import '@/app/styles/globals.css'
+import Pricing from '@/components/Pricing';
+
+export default async function PricingPage() {
+  return (
+    <>
+      <Pricing />
+    </>
+  )
+}
